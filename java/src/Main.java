a9c53543fe9369eed66420ab14ffed7052cf271d
unins000exe <stain.roman@mail.ru>
2024-02-15 13:26:19 +0400

    feature | Заготовка под запуск заданий

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String task;
+
+        loop:
+        while (true) {
+            System.out.print("""
+                    1 - Вывести Hello, World!
+                    2 - Задание 2
+                    3 - Задание 3
+                    Введите номер задания:\s""");
+            task = sc.nextLine();
+
+            switch(task) {
+                case "1":
+                    A1.hello();
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                default:
+                    System.out.println("Завершение работы программы.");
+                    break loop;
+            }
+        sc.close();
+        }
     }
 }
