6e0dd28754509a29c578023df8b00c7535369ac2
Romario <roman20011102@gmail.com>
2024-02-25 20:39:16 +0400

    feature | Функции атаки, хэш

--- src/main/java/sgu/ru/Main.java
+++ src/main/java/sgu/ru/Main.java
@@ -3,6 +3,11 @@ package main.java.sgu.ru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Knight c1 = new Knight("Рыцарь", 10, 3, 10);
+        Character c2 = new Character("Монстр", 15, 3);
+        Wizard c3 = new Wizard("Маг", 5, 3, 30);
+
+        c1.get_attacked(c3);
+        c3.get_attacked(c1);
     }
 }
